e10dc183289a70a33780a27927786062d078648d
Max Ort <43018776+mortman2299@users.noreply.github.com>
2020-01-27 17:59:16 -0600

    Update ArdConnect.java

--- ArdConnect.java
+++ ArdConnect.java
@@ -11,157 +11,7 @@ class AutoTurnOn extends TimerTask{
 	}
 }
 
-class ArdConnect{
-	public static void startAutoTurnOnTimer(){
-		Timer timer = new Timer();
-		TimerTask task = new AutoTurnOn(); 
-		timer.schedule(task, 0, 30000); 
-	}
-	
-	public static void sendShock(int intensity, int time) {
-		from.inShock = true;
-		IODevice device = new FirmtaDevice("Com4");
-		device.start();
-		divice.ensureInitializationIsDone();
-		
-		try{
-			device.getPin(9).setMode(Pin.Mode.INPUT);
-			if (intensity == 2) {
-				device.getPin(5).setMode(Pin.Mode.OUTPUT);
-				device.getPin(5).setValue(350);
-				device.getpin(7).setMode(Pin.Mode.INPUT);
-				Thread.sleep(50);
-				
-				divice.getPin(5).setMode(Pin.Mode.INPUT);
-				Thread.sleep(25);
-				
-			} else if (intensity == 3) {
-				device.getPin(5).setMode(Pin.Mode.OUTPUT);
-				device.getPin(5).setValue(350);
-				device.getpin(7).setMode(Pin.Mode.INPUT);
-				Thread.sleep(50);
-				
-				device.getPin(5).setMode(Pin.Mode.INPUT);
-				Thread.sleep(50);
-				
-				device.getPin(5).setMode(Pin.Mode.OUTPUT);
-				device.getPin(5).setValue(350);
-				device.getpin(7).setMode(Pin.Mode.INPUT);
-				Thread.sleep(50);
-				
-				device.getPin(5).setMode(Pin.Mode.INPUT);
-				Thread.sleep(25);
-			}
-			
-			device.getPin(1).setMode(Pin.Mode.OUTPUT);
-			device.getPin(1).setValue(350);
-			device.getpin(3).setMode(Pin.Mode.INPUT);
-			
-			Thread.sleep(time*50)
-				
-			device.getPin(1).setMode(Pin.Mode.INPUT);
-			
-			if (intensity == 2) {
-				device.getPin(9).setMode(Pin.Mode.OUTPUT);
-				device.getPin(9).setValue(350);
-				device.getpin(11).setMode(Pin.Mode.INPUT);
-				Thread.sleep(50);
-				
-				device.getPin(9).setMode(Pin.Mode.INPUT);
-				Thread.sleep(25);
-				
-			} else if (intensity == 3) {
-				device.getPin(9).setMode(Pin.Mode.OUTPUT);
-				device.getPin(9).setValue(350);
-				device.getpin(11).setMode(Pin.Mode.INPUT);
-				Thread.sleep(50);
-				
-				device.getPin(9).setMode(Pin.Mode.INPUT);
-				Thread.sleep(50);
-				
-				device.getPin(9).setMode(Pin.Mode.OUTPUT);
-				device.getPin(9).setValue(350);
-				device.getpin(11).setMode(Pin.Mode.INPUT);
-				Thread.sleep(50);
-				
-				device.getPin(9).setMode(Pin.Mode.INPUT);
-				Thread.sleep(25);
-			}
-		} catch (IOException e){
-			e.printStackTrace();
-		}
-						   
-	}
-	
-	public static void runShock(int intensity, int time, IODevice device){
-		try{
-			if (intensity == 2) {
-				device.getPin(5).setMode(Pin.Mode.OUTPUT);
-				device.getPin(5).setValue(350);
-				device.getpin(7).setMode(Pin.Mode.INPUT);
-				Thread.sleep(50);
-				
-				divice.getPin(5).setMode(Pin.Mode.INPUT);
-				Thread.sleep(25);
-			} else if (intensity == 3) {
-				device.getPin(5).setMode(Pin.Mode.OUTPUT);
-				device.getPin(5).setValue(350);
-				device.getpin(7).setMode(Pin.Mode.INPUT);
-				Thread.sleep(50);
-				
-				device.getPin(5).setMode(Pin.Mode.INPUT);
-				Thread.sleep(50);
-				
-				device.getPin(5).setMode(Pin.Mode.OUTPUT);
-				device.getPin(5).setValue(350);
-				device.getpin(7).setMode(Pin.Mode.INPUT);
-				Thread.sleep(50);
-				
-				device.getPin(5).setMode(Pin.Mode.INPUT);
-				Thread.sleep(25);
-			}
-			
-			device.getPin(1).setMode(Pin.Mode.OUTPUT);
-			device.getPin(1).setValue(350);
-			device.getpin(3).setMode(Pin.Mode.INPUT);
-			
-			Thread.sleep(time*50)
-				
-			device.getPin(1).setMode(Pin.Mode.INPUT);
-			
-			if (intensity == 2) {
-				device.getPin(9).setMode(Pin.Mode.OUTPUT);
-				device.getPin(9).setValue(350);
-				device.getpin(11).setMode(Pin.Mode.INPUT);
-				Thread.sleep(50);
-				
-				device.getPin(9).setMode(Pin.Mode.INPUT);
-				Thread.sleep(25);
-				
-			} else if (intensity == 3) {
-				device.getPin(9).setMode(Pin.Mode.OUTPUT);
-				device.getPin(9).setValue(350);
-				device.getpin(11).setMode(Pin.Mode.INPUT);
-				Thread.sleep(50);
-				
-				device.getPin(9).setMode(Pin.Mode.INPUT);
-				Thread.sleep(50);
-				
-				device.getPin(9).setMode(Pin.Mode.OUTPUT);
-				device.getPin(9).setValue(350);
-				device.getpin(11).setMode(Pin.Mode.INPUT);
-				Thread.sleep(50);
-				
-				device.getPin(9).setMode(Pin.Mode.INPUT);
-				Thread.sleep(25);
-			}
-		} catch (IOException e){
-			e.printStackTrace();
-		}
-		from.inShock = false;
-	}
-						   
-}
+
 
 public class ArdConnect extends JArduino {
 	public ArdConnect(String port) {
@@ -177,17 +27,15 @@ public class ArdConnect extends JArduino {
 		pinMode(DigitalPin.PIN_5, PinMode.OUTPUT);
 	}
 	
-	@Override
-	protected void loop() {
-		// set the LED on
-		digitalWrite(DigitalPin.PIN_12, DigitalState.HIGH);
-		delay(1000); // wait for a second
-		// set the LED off
-		digitalWrite(DigitalPin.PIN_12, DigitalState.LOW);
-		delay(1000); // wait for a second
+	
+	public static void startAutoTurnOnTimer(){
+		Timer timer = new Timer();
+		TimerTask task = new AutoTurnOn(); 
+		timer.schedule(task, 0, 30000); 
 	}
-
-	public static void run(int intensity, int time){
+	
+	public static void sendShock(int intensity, int time){
+		from.inShock = true;
 		if (intensity == 2){
 			digitalWrite(DigitalPin.PIN_3, DigitalState.HIGH);
 			delay(50);
@@ -227,6 +75,7 @@ public class ArdConnect extends JArduino {
 			digitalWrite(DigitalPin.PIN_5, DigitalState.LOW);
 			delay(25);
 		}
+		from.inShock = false;
 	}
 	
 	public static void main(String[] args) {
