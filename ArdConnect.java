42860fc3fd7dc28ed53c3e3c269453c2e203f9cf
Max Ort <43018776+mortman2299@users.noreply.github.com>
2020-01-27 10:30:01 -0600

    Update ArdConnect.java

--- ArdConnect.java
+++ ArdConnect.java
@@ -1,20 +1,78 @@
 class ArdConnect {
-	public static sendShock(int intensity, int time) {
-		IODevice device = new FirmtaDevice("Com4);
+	public static void sendShock(int intensity, int time) {
+		IODevice device = new FirmtaDevice("Com4");
 		device.start();
 		divice.ensureInitializationIsDone();
 		
-		turnOn(intensity, time)
+		runShock(intensity, time, device);
+						   
 	}
 	
-	public static turnOn(int intensity, int time, IODevice device){
+	public static void runShock(int intensity, int time, IODevice device){
 		try{
-			device.getPin(7).setMode(Pin.Mode.OUTPUT);
-			divice.getPin(7).setValue(350);
-			divice.getpin(11).setMode(Pin.Mode.INPUT);
+			if (intensity == 2) {
+				device.getPin(5).setMode(Pin.Mode.OUTPUT);
+				device.getPin(5).setValue(350);
+				device.getpin(7).setMode(Pin.Mode.INPUT);
+				Thread.sleep(50);
+				
+				divice.getPin(5).setMode(Pin.Mode.INPUT);
+				Thread.sleep(25);
+			} else if (intensity == 3) {
+				device.getPin(5).setMode(Pin.Mode.OUTPUT);
+				device.getPin(5).setValue(350);
+				device.getpin(7).setMode(Pin.Mode.INPUT);
+				Thread.sleep(50);
+				
+				device.getPin(5).setMode(Pin.Mode.INPUT);
+				Thread.sleep(50);
+				
+				device.getPin(5).setMode(Pin.Mode.OUTPUT);
+				device.getPin(5).setValue(350);
+				device.getpin(7).setMode(Pin.Mode.INPUT);
+				Thread.sleep(50);
+				
+				device.getPin(5).setMode(Pin.Mode.INPUT);
+				Thread.sleep(25);
+			}
 			
+			device.getPin(1).setMode(Pin.Mode.OUTPUT);
+			device.getPin(1).setValue(350);
+			device.getpin(3).setMode(Pin.Mode.INPUT);
+			
+			Thread.sleep(time*50)
+				
+			device.getPin(1).setMode(Pin.Mode.INPUT);
+			
+			if (intensity == 2) {
+				device.getPin(9).setMode(Pin.Mode.OUTPUT);
+				device.getPin(9).setValue(350);
+				device.getpin(11).setMode(Pin.Mode.INPUT);
+				Thread.sleep(50);
+				
+				device.getPin(9).setMode(Pin.Mode.INPUT);
+				Thread.sleep(25);
+				
+			} else if (intensity == 3) {
+				device.getPin(9).setMode(Pin.Mode.OUTPUT);
+				device.getPin(9).setValue(350);
+				device.getpin(11).setMode(Pin.Mode.INPUT);
+				Thread.sleep(50);
+				
+				device.getPin(9).setMode(Pin.Mode.INPUT);
+				Thread.sleep(50);
+				
+				device.getPin(9).setMode(Pin.Mode.OUTPUT);
+				device.getPin(9).setValue(350);
+				device.getpin(11).setMode(Pin.Mode.INPUT);
+				Thread.sleep(50);
+				
+				device.getPin(9).setMode(Pin.Mode.INPUT);
+				Thread.sleep(25);
+			}
 		} catch (IOException e){
 			e.printStackTrace();
 		}
 	}
+						   
 }
