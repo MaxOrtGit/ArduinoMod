3310b306cc51c1a4bfc37c7bb8f9c1d195040261
Max Ort <43018776+mortman2299@users.noreply.github.com>
2020-01-27 16:39:14 -0600

    Update ArdConnect.java

--- ArdConnect.java
+++ ArdConnect.java
@@ -162,3 +162,40 @@ class ArdConnect{
 	}
 						   
 }
+
+public class ArdConnect extends JArduino {
+
+public ArdConnect(String port) {
+    super(port);
+}
+
+@Override
+protected void setup() {
+    // initialize the digital pin as an output.
+    // Pin 13 has an LED connected on most Arduino boards:
+    pinMode(DigitalPin.PIN_12, PinMode.OUTPUT);
+}
+
+@Override
+protected void loop() {
+    // set the LED on
+    digitalWrite(DigitalPin.PIN_12, DigitalState.HIGH);
+    delay(1000); // wait for a second
+    // set the LED off
+    digitalWrite(DigitalPin.PIN_12, DigitalState.LOW);
+    delay(1000); // wait for a second
+}
+
+public static void main(String[] args) {
+    String serialPort;
+    if (args.length == 1) {
+        serialPort = args[0];
+    } else {
+        serialPort = Serial4JArduino.selectSerialPort();
+    }
+    JArduino arduino = new ArdConnect(serialPort);
+    arduino.runArduinoProcess();
+}
+}
+
+
